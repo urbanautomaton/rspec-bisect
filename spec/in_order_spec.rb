f2ce30637e7010daaa3985d9a2d879c289be43e9
Simon Coffey <simon@urbanautomaton.com>
2015-03-01 23:48:28 +0000

    Profiling, meh

---
+++ spec/in_order_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+RSpec.describe "examples only pass when they are run in order" do
+  before(:context) { @list = [] }
+
+  it "passes when run second" do
+    @list << 2
+    # expect(@list).to eq([1, 2])
+  end
+
+  it "sets a fish" do
+    ::FISH = :chub
+  end
+
+  it "passes when run third" do
+    @list << 3
+    # expect(@list).to eq([1, 2, 3])
+  end
+
+  describe "another group" do
+    it "passes all the time" do
+      expect(true).to be_truthy
+    end
+  end
+end
---
+++ spec/other_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe "some other group" do
+  it "does a thing" do
+    expect(false).to be_falsey
+  end
+end
+
+RSpec.describe "a second group in a file" do
+  it "dislikes fish" do
+    expect(defined?(::FISH)).to be_falsey
+  end
+end
